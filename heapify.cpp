4a3c4f04348200a445695314fdb8b26f62f6178f
Usman Ashfaq <156636786+Usman-Ashfaq@users.noreply.github.com>
2025-07-09 23:59:33 +0500

    Update heapify.cpp

--- heapify.cpp
+++ heapify.cpp
@@ -49,7 +49,7 @@ void printArray(int arr[], int n) {
     cout << endl;
 }
 
-// ---------- Driver Code ----------
+
 int main() {
     int arr[] = { 12, 11, 13, 5, 6, 7 };
     //int n = sizeof(arr) / sizeof(arr[0]);
